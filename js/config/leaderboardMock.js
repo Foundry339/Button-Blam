775cd85d7c0d5191d713a9ec66c84b7675e165e0
foundryweblab <foundryweblab@gmail.com>
2026-08-14 12:27:24 -0400

    ad placeholders

--- js/config/leaderboardMock.js
+++ js/config/leaderboardMock.js
@@ -2,7 +2,7 @@
 // by a real "top scores" API response.
 export const MOCK_LEADERBOARD = [
   { name: "ThatsWhatSheSaid69", clicks: 482_392 },
-  { name: "ClickingDad", clicks: 381_204 },
+  { name: "BlamMakeHerBlam", clicks: 381_204 },
   { name: "AvrilLavigneFan4", clicks: 294_882 },
   { name: "GoblinDeezButtons", clicks: 241_109 },
   { name: "PickleRick235", clicks: 198_431 },
