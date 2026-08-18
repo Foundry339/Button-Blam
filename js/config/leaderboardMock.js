7caed34b83bd1786c9c76109a61c36fb68032b62
foundryweblab <foundryweblab@gmail.com>
2026-08-17 20:28:00 -0400

    Update leaderboardMock.js

--- js/config/leaderboardMock.js
+++ js/config/leaderboardMock.js
@@ -2,7 +2,7 @@
 // by a real "top scores" API response.
 export const MOCK_LEADERBOARD = [
   { name: "Tcelly4eva", clicks: 482_392 },
-  { name: "BlamMakeHerBlam", clicks: 381_204 },
+  { name: "GordoTheBlamLord", clicks: 381_204 },
   { name: "AvrilLavigneFan4", clicks: 294_882 },
   { name: "GoblinDeezButtons", clicks: 241_109 },
   { name: "PickleRick235", clicks: 198_431 },
